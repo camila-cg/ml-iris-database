11e30b132bbef8188e524a9658cb8c208168bae7
camila.gusmao <camilagusmao08@gmail.com>
2021-04-13 17:19:28 -0300

    Ajustando leitura do dataset

--- index.py
+++ index.py
@@ -4,23 +4,20 @@ from sklearn.datasets import load_iris
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-def loadDataset():
-    #Lendo dataset a partir da url
+def loadDatasetByUrl():
     url = 'http://archive.ics.uci.edu/ml/machine-learning-databases/iris/iris.data'
     attributes = ["sepal_length", "sepal_width", "petal_length", "petal_width", "class"]
     dataset = pd.read_csv(url, names = attributes)
-    #dataset.columns = attributes
+    return dataset
 
-    #Lendo dataset a partir do sklearn
-    #data = load_iris()
-    #X= data.data
-    #y= data.target
-    # target: 0 = setosa, 1 = versicolor, 2 = virginica
-    #dataset = pd.DataFrame(data= np.c_[data['data'], data['target']], columns= data['feature_names'] + ['class'])
-    #print (data.target[[10, 25, 50]])
+def loadDatasetBySklearn():
+    data = load_iris()
+    # https://numpy.org/devdocs/reference/generated/numpy.c_.html
+    dataset = pd.DataFrame(data= np.c_[data['data'], data['target']], columns= data['feature_names'] + ['class'])
     #print(list(data.target_names))
-    #print(data.head())
-
+    dataset['class'] = dataset["class"].replace(0, 'setosa')
+    dataset['class'] = dataset["class"].replace(1, 'versicolor')
+    dataset['class'] = dataset["class"].replace(2, 'virginica')
     #print(dataset.head())
     #print(dataset.groupby('class').size())
     return dataset
@@ -28,27 +25,28 @@ def loadDataset():
 
 def plotBoxplot(dataset):
     fig, axs = plt.subplots(2, 2)
-    fn = ["sepal_length", "sepal_width", "petal_length", "petal_width"]
-    cn = ['Iris-setosa', 'Iris-versicolor', 'Iris-virginica'] # ['setosa', 'versicolor', 'virginica']
-    sns.boxplot(x = 'class', y = 'sepal_length', data = dataset, order = cn, ax = axs[0,0]);
-    sns.boxplot(x = 'class', y = 'sepal_width', data = dataset, order = cn, ax = axs[0,1]);
-    sns.boxplot(x = 'class', y = 'petal_length', data = dataset, order = cn, ax = axs[1,0]);
-    sns.boxplot(x = 'class', y = 'petal_width', data = dataset,  order = cn, ax = axs[1,1]);
-    fig.tight_layout(pad=1.0);
+    #fn = ["sepal_length", "sepal_width", "petal_length", "petal_width"]
+    #cn = ['Iris-setosa', 'Iris-versicolor', 'Iris-virginica']
+    cn = ['setosa', 'versicolor', 'virginica']
+    sns.boxplot(x = 'class', y = 'sepal length (cm)', data = dataset, order = cn, ax = axs[0,0])
+    sns.boxplot(x = 'class', y = 'sepal width (cm)', data = dataset, order = cn, ax = axs[0,1])
+    sns.boxplot(x = 'class', y = 'petal length (cm)', data = dataset, order = cn, ax = axs[1,0])
+    sns.boxplot(x = 'class', y = 'petal width (cm)', data = dataset,  order = cn, ax = axs[1,1])
+    fig.tight_layout(pad=1.0)
     plt.show()
 
 def plotHistogram(dataset):
     n_bins = 10
     fig, axs = plt.subplots(2, 2)
-    axs[0,0].hist(dataset['sepal_length'], bins = n_bins);
-    axs[0,0].set_title('Sepal Length');
-    axs[0,1].hist(dataset['sepal_width'], bins = n_bins);
-    axs[0,1].set_title('Sepal Width');
-    axs[1,0].hist(dataset['petal_length'], bins = n_bins);
-    axs[1,0].set_title('Petal Length');
-    axs[1,1].hist(dataset['petal_width'], bins = n_bins);
-    axs[1,1].set_title('Petal Width');
-    fig.tight_layout(pad=1.0);
+    axs[0,0].hist(dataset['sepal length (cm)'], bins = n_bins)
+    axs[0,0].set_title('Sepal Length')
+    axs[0,1].hist(dataset['sepal width (cm)'], bins = n_bins)
+    axs[0,1].set_title('Sepal Width')
+    axs[1,0].hist(dataset['petal length (cm)'], bins = n_bins)
+    axs[1,0].set_title('Petal Length')
+    axs[1,1].hist(dataset['petal width (cm)'], bins = n_bins)
+    axs[1,1].set_title('Petal Width')
+    fig.tight_layout(pad=1.0)
     plt.show()
 
 def plotScatterPlot(dataset):
@@ -59,7 +57,7 @@ def plotScatterPlot(dataset):
 
 def main():
     # 1. Carregue o dataset iris como um dataframe
-    dataset = loadDataset()
+    dataset = loadDatasetBySklearn()
 
     # 2. Calcule, para cada atributo, as estatísticas média, mediana, desvio-padrão, Q1, Q3, obliquidade e curtose. Apontem nos resultados de vocês qual(is) biblioteca(s) vcs utilizaram.
     print(dataset.describe())
