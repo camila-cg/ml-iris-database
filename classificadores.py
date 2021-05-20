46f85c5588e26ddd5dddeb8e836c57eb68f0311f
camila.gusmao <camilagusmao08@gmail.com>
2021-05-19 23:17:57 -0300

    Corrigindo identação

--- classificadores.py
+++ classificadores.py
@@ -21,56 +21,60 @@ import pandas as pd
 
 def calcularMetricas(k, y_test, y_pred):
   scoring = {
-      'k': k,
-      'accuracy': accuracy_score(y_test, y_pred),
-      'precision_micro': precision_score(y_test, y_pred, average = 'micro', zero_division=1),
-      'precision_macro': precision_score(y_test, y_pred, average = 'macro', zero_division=1),
-      'precision_weighted': precision_score(y_test, y_pred, average = 'weighted', zero_division=1),
-      'recall_micro': recall_score(y_test, y_pred, average = 'micro', zero_division=1),
-      'recall_macro': recall_score(y_test, y_pred, average = 'macro', zero_division=1),
-      'recall_weighted': recall_score(y_test, y_pred, average = 'weighted', zero_division=1),
-      'f1_micro': f1_score(y_test, y_pred, average = 'micro', zero_division=1),
-      'f1_macro': f1_score(y_test, y_pred, average = 'macro', zero_division=1),
-      'f1_weighted': f1_score(y_test, y_pred, average = 'weighted', zero_division=1)
+    'k': k,
+    'accuracy': accuracy_score(y_test, y_pred),
+    'precision_micro': precision_score(y_test, y_pred, average = 'micro', zero_division=1),
+    'precision_macro': precision_score(y_test, y_pred, average = 'macro', zero_division=1),
+    'precision_weighted': precision_score(y_test, y_pred, average = 'weighted', zero_division=1),
+    'recall_micro': recall_score(y_test, y_pred, average = 'micro', zero_division=1),
+    'recall_macro': recall_score(y_test, y_pred, average = 'macro', zero_division=1),
+    'recall_weighted': recall_score(y_test, y_pred, average = 'weighted', zero_division=1),
+    'f1_micro': f1_score(y_test, y_pred, average = 'micro', zero_division=1),
+    'f1_macro': f1_score(y_test, y_pred, average = 'macro', zero_division=1),
+    'f1_weighted': f1_score(y_test, y_pred, average = 'weighted', zero_division=1)
   }
   return scoring
 
+
 def classificadorKNN(X_train, X_test, y_train, y_test):
-    print("classificadorKNN")
-    valores_k = [1,3,5,7,9]
-    scores = {}
-    scores_list = []
-    resultados = list()
-    for k in valores_k:
-        modelo = KNeighborsClassifier(n_neighbors=k)
-        modelo.fit(X_train, y_train)
-        y_pred = modelo.predict(X_test)
-        
-        #TODO
-        resultados.append(calcularMetricas(k, y_test, y_pred))
-
-        print(accuracy_score(y_test, y_pred))
-        scores[k] = accuracy_score(y_test, y_pred)
-        scores_list.append(accuracy_score(y_test, y_pred))
+  print("classificadorKNN")
+  valores_k = [1,3,5,7,9]
+  scores = {}
+  scores_list = []
+  resultados = list()
+  for k in valores_k:
+    modelo = KNeighborsClassifier(n_neighbors=k)
+    modelo.fit(X_train, y_train)
+    y_pred = modelo.predict(X_test)
     
-    print(resultados)
-    plt.plot(valores_k, scores_list)
-    plt.xlabel('Valor de k para o KNN')
-    plt.ylabel('Acurácia')
-    plt.show()
+    #TODO
+    resultados.append(calcularMetricas(k, y_test, y_pred))
+
+    print(accuracy_score(y_test, y_pred))
+    scores[k] = accuracy_score(y_test, y_pred)
+    scores_list.append(accuracy_score(y_test, y_pred))
+  
+  print(resultados)
+  plt.plot(valores_k, scores_list)
+  plt.xlabel('Valor de k para o KNN')
+  plt.ylabel('Acurácia')
+  plt.show()
 
 
-def gridSearch(X_train, X_test, y_train, y_test):
+def classificadorSVM(X_train, X_test, y_train, y_test):
+  print("classificadorSVM")
   modelo = SVC(random_state=10, tol=0.0001)
   #print(modelo.get_params().keys())
 
+  '''
   parameters = {
-      'kernel':('linear', 'rbf', 'sigmoid', 'poly'),
-      'C':[0.025, 0.001, 0.05, 0.01, 0.1, 1, 4, 10, 50, 100, 1000],
-      'gamma': [0.001, 0.01, 0.1, 1, 10, 50],
-      'coef0': [0.1, 1, 10, 100],
-      'degree': [1,2,3]
+    'kernel':('linear', 'rbf', 'sigmoid', 'poly'),
+    'C':[0.025, 0.001, 0.05, 0.01, 0.1, 1, 4, 10, 50, 100, 1000],
+    'gamma': [0.001, 0.01, 0.1, 1, 10, 50],
+    'coef0': [0.1, 1, 10, 100],
+    'degree': [1,2,3]
   }
+  '''
 
   param_grid = [
     {'C':[0.025, 0.001, 0.05, 0.01, 0.1, 1, 4, 10, 50, 100, 1000], 'kernel': ['linear']},
@@ -112,50 +116,40 @@ def gridSearch(X_train, X_test, y_train, y_test):
     
   pd.set_option('max_columns', 200)
   result_grid_search = pd.DataFrame(clf.cv_results_)
-  pd.DataFrame(data = result_grid_search).to_csv('result_grid_search_svm-2.csv', encoding='utf-8')
+  pd.DataFrame(data = result_grid_search).to_csv('result_grid_search_svm.csv', encoding='utf-8')
 
-
-
-
+  #modelo.fit(X_train, y_train)
+  #pred=modelo.predict(X_test)
+  #print(confusion_matrix(y_test, pred))
+  #print(classification_report(y_test, pred))
 
 
 def classificadorAD():
-    print("classificadorAD")
+  print("classificadorAD")
 
 
 def classificadorNB(X_train, X_test, y_train, y_test):
-    print("classificadorNB")
-    n_class=GaussianNB()
-    n_class.fit(X_train, y_train)
-    y_pred_bayes=n_class.predict(X_test)
-
-def classificadorSVM(X_train, X_test, y_train, y_test):
-    print("classificadorSVM")
-    modelo=SVC()
-    modelo.fit(X_train, y_train)
-    pred=modelo.predict(X_test)
-    print(confusion_matrix(y_test, pred))
-    print(classification_report(y_test, pred))
-
-    #Variar os parâmetros de SVM e MLP usando a função Randomized Search do Scikit (https://scikit-learn.org/stable/modules/generated/sklearn.model_selection.RandomizedSearchCV.html)
-    #ou o GridSearchCV (https://scikit-learn.org/stable/modules/generated/sklearn.model_selection.GridSearchCV.html).
+  print("classificadorNB")
+  n_class=GaussianNB()
+  n_class.fit(X_train, y_train)
+  y_pred_bayes=n_class.predict(X_test)
 
 
 def classificadorMLP():
-    print("classificadorMLP")
+  print("classificadorMLP")
+
 
 def classificadores(dataset):
-    X=dataset.drop('class', axis=1)
-    y=dataset['class']
-    #print(y.head())
-    #print(X.head())
+  X=dataset.drop('class', axis=1)
+  y=dataset['class']
+  #print(y.head())
+  #print(X.head())
 
-    # Holdout com 1/3 dos dados para teste (50 registros)
-    # e 2/3 para treinamento (100 registros)
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=50)
-    
-    #classificadorKNN(X_train, X_test, y_train, y_test)
-    #classificadorSVM(X_train, X_test, y_train, y_test)
-    gridSearch(X_train, X_test, y_train, y_test)
+  # Holdout com 1/3 dos dados para teste (50 registros)
+  # e 2/3 para treinamento (100 registros)
+  X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=50)
+  
+  #classificadorKNN(X_train, X_test, y_train, y_test)
+  classificadorSVM(X_train, X_test, y_train, y_test)
     
 
